bc7a5b24f8e7e0ef70bd450f6ba7ff3ff2188fdd
chakraborty-ani-2 <chakrabortyaniket20800@gmail.com>
2024-10-18 23:44:59 +0530

    fixed another linting bug in App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 // COMPONENTS
 import MainPage from './components/MainPage/MainPage';
